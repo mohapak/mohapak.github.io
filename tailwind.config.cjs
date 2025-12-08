8244e0514f5d2b3730111f4143a8fda0754b9648
pakravanmohammadmehdi <pakravanmohammad.eco@gmail.com>
2025-12-08 17:06:44 +0330

    modified:   index.html 	modified:   public/network.html 	modified:   src/index.css 	modified:   tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -10,6 +12,10 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Plus Jakarta Sans"', ...defaultTheme.fontFamily.sans],
+        display: ['"Source Serif 4"', ...defaultTheme.fontFamily.serif],
+      },
       transitionDuration: {
         '250': '250ms',
         '350': '350ms',
